4e74d5a9015835db8270455683a26c98a6d85d82
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-01-12 14:30:27 -0600

    still on the same display bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
 const start = $('#submit');
-const search = document.querySelector('#search');
+const search = $('#search');
 let lat = '';
 let lon = '';
 let city = '';
 const urlWeather = 'api.openweathermap.org/data/2.5/forecast?lat={' + lat + '}&lon={' + lon + '}&appid={979baed4769ff26eb4c2d6a4cb12cc90}';
 const urlGeocash = 'http://api.openweathermap.org/geo/1.0/direct?q={' + city + '}&limit={5}&appid={979baed4769ff26eb4c2d6a4cb12cc90}';
 
-start.on('click', function () {
-    console.log(search.input)
-});
+start.on('click', function (event) {
+    event.preventDefault();
 
-// fetch(urlGeocash)
+    // city = search.val
+    console.log(search.form)
+    // fetch(urlGeocash)
 //     .then(function (response) {
 //         return response.json();
 //      })
 //      .then(function (data) {
-
+//          lat = data.lat;
+//          lon = data.lon;
 //      });
+});
+
+
