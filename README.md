efc1abe6d16ba7cb5cacd102ab0317b23a226c97
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-27 17:36:49 -0600

    added local stoarge functions

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wether Call API
+Just enter a city to get the local weather
+
+## Function
+
+This webpage retuns weather data on a city from an api call.
+All the user needs to do is enter the city name into the search bar and click search
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const start = $('#submit');
+const start = document.getElementById('submit');
 const search = document.getElementById('search');
+const searchedCities = document.getElementById('cities');
 var lat;
 var lon;
 var city;
@@ -8,9 +9,11 @@ const urlGeocash = 'http://api.openweathermap.org/geo/1.0/direct?q='
 const weatherApiKey = '&appid=979baed4769ff26eb4c2d6a4cb12cc90';
 const geocashApiKey = '&limit=5&appid=fe28c6dbe1ce362b35e1b5d40fdbcab2';
 var urlWeatherApi;
+var searchTerms = [];
 
-start.on('click', async function (event) {
+start.addEventListener('click', async function (event) {
     city = search.value
+    searchTerms.push(city)
     const urlGeocashApi = urlGeocash + city + geocashApiKey
     console.log(urlGeocashApi)
     const geocashResponse = await fetch(urlGeocashApi);
@@ -20,8 +23,33 @@ start.on('click', async function (event) {
     lon = geocashData[0].lon;
     urlWeatherApi = urlWeather + lat + '&lon=' + lon + weatherApiKey;
     console.log(urlWeatherApi);
-    
+
     const weatherResponse = await fetch(urlWeatherApi);
 });
 
+function setStorage() {
+    localStorage.setItem('searchTerms', JSON.stringify(searchTerms))
+};
 
+async function retrieveStorage() {
+    try {
+        const response = await localStorage.getItem('searchTerms')
+        searchTerms = JSON.parse(response)
+        if (!searchTerms.length) { return }
+        else {
+            searchTerms.forEach(item => {
+                var div = document.createElement('div')
+                div.innerHTML = item
+                searchedCities.appendChild(div)
+            });
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+
+
+    return searchTerms
+};
+
+retrieveStorage();
